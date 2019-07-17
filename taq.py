81e12f844bae128e1a98e6a25832b58a1f1d1f2f
Adam <30326744+AlgoTrader5@users.noreply.github.com>
2019-07-17 12:24:30 -0500

    Update taq.py

--- taq.py
+++ taq.py
@@ -43,7 +43,9 @@ q.open()
 async def trade(feed, pair, id, timestamp, side, amount, price):
 	hwt = str(datetime.utcnow().isoformat()).replace("T","D").replace("-",".")
 	ts = str(datetime.fromtimestamp(timestamp).isoformat()).replace("T","D").replace("-",".")
-	q.sendSync(f"`trades insert (`timestamp${hwt};`timestamp${ts};`$\"{pair}\";`{side};{amount};{price};`{feed};{id})")
+	qStr = f"`trades insert (`timestamp${hwt};`timestamp${ts};`$\"{pair}\";`{side};{amount};{price};`{feed};{id})"
+	print(qStr)
+	q.sendSync(qStr)
 
 
 def main():
