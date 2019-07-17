9c58cda525dc03c453ef3d36420040584d957231
Adam <30326744+AlgoTrader5@users.noreply.github.com>
2019-07-17 16:11:03 -0500

    Update taq.py

--- taq.py
+++ taq.py
@@ -1,11 +1,9 @@
 '''
 Records trade and quote data to kdb+
-
 exchange and product subscriptions configured in subscriptions.yaml 
-
 modified from https://kx.com/blog/combining-high-frequency-cryptocurrency-venue-data-using-kdb/
 '''
-from cryptofeed.callback import TradeCallback
+from cryptofeed.callback import TradeCallback, BookCallback
 from cryptofeed import FeedHandler
 from cryptofeed.exchanges import Coinbase
 from cryptofeed.defines import TRADES
@@ -14,26 +12,33 @@ from datetime import datetime
 from kdb_client import KdbClient
 
 
-
 async def trade(feed, pair, id, timestamp, side, amount, price):
-	hwt = str(datetime.utcnow().isoformat()).replace("T","D").replace("-",".")
-	ts = str(datetime.fromtimestamp(timestamp).isoformat()).replace("T","D").replace("-",".")
-	kdb_client.exequery(f"`trades insert (`timestamp${hwt};`timestamp${ts};`{feed};`$\"{pair}\";`{side};{amount};{price};{id})")
-	
+    hwt = str(datetime.utcnow().isoformat()).replace("T","D").replace("-",".")
+    ts = str(datetime.fromtimestamp(timestamp).isoformat()).replace("T","D").replace("-",".")
+    kdb_client.exequery(f"`trades insert (`timestamp${hwt};`timestamp${ts};`{feed};`$\"{pair}\";`{side};{amount};{price};{id})")
+    
+async def book(feed, pair, book, timestamp):
+    hwt = str(datetime.utcnow().isoformat()).replace("T","D").replace("-",".")
+    ts = str(datetime.fromtimestamp(timestamp).isoformat()).replace("T","D").replace("-",".")
+    bid_price = list(book['bid'])[-1]
+    bid_size = float(book['bid'][bid_price])
+    ask_price = list(book['ask'])[0]
+    ask_size = float(book['ask'][ask_price])
+    kdb_client.exequery(f"`quotes insert (`timestamp${hwt};`timestamp${ts};`{feed};`$\"{pair}\";{bid_size};{bid_price};{ask_price};{ask_size})")
 
+    
 def main():
-	try:
-		kdb_client = KdbClient(host='localhost', port=5002)
-		
-		f = FeedHandler()
-		f.add_feed(Coinbase(channels=[TRADES], pairs=['ETH-USD'], callbacks={TRADES: TradeCallback(trade)}))
-		f.run()
-	finally:
-		kdb_client.close()
-		
+        try:
+            kdb_client = KdbClient(host='localhost', port=5002)
+            f = FeedHandler()
+            f.add_feed(Coinbase(channels=[TRADES], pairs=['ETH-USD'], callbacks={TRADES: TradeCallback(trade)}))
+            f.run()
+        finally:
+            kdb_client.close()
+        
 
 
 
 if __name__ == '__main__':
-	main()
+    main()
 
