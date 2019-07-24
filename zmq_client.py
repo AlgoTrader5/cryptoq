fdd8087b72931b20f3ebaa987d7e950202330d7f
Adam <30326744+AlgoTrader5@users.noreply.github.com>
2019-07-24 01:10:30 -0500

    Update zmq_client.py

--- zmq_client.py
+++ zmq_client.py
@@ -1,3 +1,4 @@
+import os
 import zmq
 import time
 import argparse
@@ -26,12 +27,12 @@ q.open()
 
 
 
-def book_receiver(port):
-    addr = 'tcp://127.0.0.1:{}'.format(port)
+def book_receiver(port, topicfilter=b'book'):
+    addr = f'tcp://127.0.0.1:{port}'
     print(f'book receiver address: {addr}')
     ctx = zmq.Context.instance()
     s = ctx.socket(zmq.SUB)
-    s.setsockopt(zmq.SUBSCRIBE, b'')
+    s.setsockopt(zmq.SUBSCRIBE, topicfilter)
 
     s.bind(addr)
     while True:
@@ -43,12 +44,12 @@ def book_receiver(port):
             print(f"Error executing query {qStr} against server. {e}")
 
 
-def trade_receiver(port):
-    addr = 'tcp://127.0.0.1:{}'.format(port)
+def trade_receiver(port, topicfilter=b'trades'):
+    addr = f'tcp://127.0.0.1:{port}'
     print(f'trade receiver address: {addr}')
     ctx = zmq.Context.instance()
     s = ctx.socket(zmq.SUB)
-    s.setsockopt(zmq.SUBSCRIBE, b'')
+    s.setsockopt(zmq.SUBSCRIBE, topicfilter)
 
     s.bind(addr)
     while True:
@@ -61,55 +62,55 @@ def trade_receiver(port):
 
 
 def main():
-        import os
+    subscriptions = read_cfg("conf//subscriptions.yaml")
+    coinbase_tickers = subscriptions['coinbase']['pairs']
+    kraken_tickers = subscriptions['kraken']['pairs']
+    binance_tickers = subscriptions['kraken']['pairs']
+
+    print(f"IPC version: {q.protocol_version}. Is connected: {q.is_connected()}")
+
+    try:
+        p1 = Process(target=trade_receiver, args=(5555,))
+        p2 = Process(target=book_receiver, args=(5556,))
         
-        subscriptions = read_cfg("conf//subscriptions.yaml")
-        coinbase_tickers = subscriptions['coinbase']['pairs']
-        kraken_tickers = subscriptions['kraken']['pairs']
-
-        print(f"IPC version: {q.protocol_version}. Is connected: {q.is_connected()}")
-
-        try:
-            p1 = Process(target=trade_receiver, args=(5555,))
-            p2 = Process(target=book_receiver, args=(5556,))
-            p3 = Process(target=trade_receiver, args=(5557,))
-            p4 = Process(target=book_receiver, args=(5558,))
-            
-            p1.start()
-            p2.start()
-            p3.start()
-            p4.start() 
-
-            f = FeedHandler()
-            
-            f.add_feed(Coinbase(
-                channels=[L2_BOOK, TRADES], 
-                pairs=coinbase_tickers, 
-                callbacks={
-                    TRADES: TradeZMQ(port=5555), 
-                    L2_BOOK: BookZMQ(depth=1, port=5556)}))
-            
-            f.add_feed(Kraken(
-                channels=[L2_BOOK, TRADES], 
-                pairs=kraken_tickers, 
-                callbacks={
-                    TRADES: TradeZMQ(port=5557), 
-                    L2_BOOK: BookZMQ(depth=1, port=5558)}))
+        p1.start()
+        p2.start()
 
 
-            f.run()
+        f = FeedHandler()
+        
+        f.add_feed(Coinbase(
+            channels=[L2_BOOK, TRADES], 
+            pairs=coinbase_tickers, 
+            callbacks={
+                TRADES: TradeZMQ(port=5555), 
+                L2_BOOK: BookZMQ(depth=1, port=5556)}))
+        
+        f.add_feed(Kraken(
+            channels=[L2_BOOK, TRADES], 
+            pairs=kraken_tickers, 
+            callbacks={
+                TRADES: TradeZMQ(port=5555), 
+                L2_BOOK: BookZMQ(depth=1, port=5556)}))
+        
+        f.add_feed(Binance(
+            channels=[L2_BOOK, TRADES], 
+            pairs=binance_tickers, 
+            callbacks={
+                TRADES: TradeZMQ(port=5555), 
+                L2_BOOK: BookZMQ(depth=1, port=5556)}))
 
-        finally:
-            p1.terminate()
-            p2.terminate()
-            p3.terminate()
-            p4.terminate()
-            
-            # save trades and quotes tables to disk
-            data_path = "d:/repos/cryptoq/data"
-            q.sendSync(f"`:{data_path}/trades set trades")
-            q.sendSync(f"`:{data_path}/quotes set quotes")
-            q.close()
+        f.run()
+
+    finally:
+        p1.terminate()
+        p2.terminate()
+        
+        # save trades and quotes tables to disk
+        data_path = "d:/repos/cryptoq/data"
+        q.sendSync(f"`:{data_path}/trades set trades")
+        q.sendSync(f"`:{data_path}/quotes set quotes")
+        q.close()
             
 if __name__ in "__main__":
     main()
