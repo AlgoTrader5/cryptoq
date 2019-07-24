ad213c69dbeb042aa5492b44cfb68b2670db5a05
Adam <30326744+AlgoTrader5@users.noreply.github.com>
2019-07-24 01:31:13 -0500

    Update zmq_client.py

--- zmq_client.py
+++ zmq_client.py
@@ -9,7 +9,7 @@ from qpython.qtype import QException
 
 from cryptofeed.backends.zmq import BookZMQ, TradeZMQ
 from cryptofeed import FeedHandler
-from cryptofeed.exchanges import Coinbase, Kraken
+from cryptofeed.exchanges import Coinbase, Kraken, Binance
 from cryptofeed.defines import TRADES, L2_BOOK
 
 from utils import read_cfg, trade_convert, book_convert
@@ -27,34 +27,21 @@ q.open()
 
 
 
-def book_receiver(port, topicfilter=b'book'):
+def receiver(port):
     addr = f'tcp://127.0.0.1:{port}'
     print(f'book receiver address: {addr}')
     ctx = zmq.Context.instance()
     s = ctx.socket(zmq.SUB)
-    s.setsockopt(zmq.SUBSCRIBE, topicfilter)
+    s.setsockopt(zmq.SUBSCRIBE, b'book')
+    s.setsockopt(zmq.SUBSCRIBE, b'trades')
 
     s.bind(addr)
     while True:
         data = s.recv_string()
-        qStr = book_convert(data)
-        try:
-            q.sendSync(qStr, param=None)
-        except QException as e:
-            print(f"Error executing query {qStr} against server. {e}")
-
-
-def trade_receiver(port, topicfilter=b'trades'):
-    addr = f'tcp://127.0.0.1:{port}'
-    print(f'trade receiver address: {addr}')
-    ctx = zmq.Context.instance()
-    s = ctx.socket(zmq.SUB)
-    s.setsockopt(zmq.SUBSCRIBE, topicfilter)
-
-    s.bind(addr)
-    while True:
-        data = s.recv_string()
-        qStr = trade_convert(data)
+        if data[0] == "b":
+            qStr = book_convert(data)
+        else:
+            qStr = trade_convert(data)
         try:
             q.sendSync(qStr, param=None)
         except QException as e:
@@ -65,16 +52,13 @@ def main():
     subscriptions = read_cfg("conf//subscriptions.yaml")
     coinbase_tickers = subscriptions['coinbase']['pairs']
     kraken_tickers = subscriptions['kraken']['pairs']
-    binance_tickers = subscriptions['kraken']['pairs']
+    binance_tickers = subscriptions['binance']['pairs']
 
     print(f"IPC version: {q.protocol_version}. Is connected: {q.is_connected()}")
 
     try:
-        p1 = Process(target=trade_receiver, args=(5555,))
-        p2 = Process(target=book_receiver, args=(5556,))
-        
-        p1.start()
-        p2.start()
+        p = Process(target=receiver, args=(5555,))
+        p.start()
 
 
         f = FeedHandler()
@@ -84,27 +68,26 @@ def main():
             pairs=coinbase_tickers, 
             callbacks={
                 TRADES: TradeZMQ(port=5555), 
-                L2_BOOK: BookZMQ(depth=1, port=5556)}))
+                L2_BOOK: BookZMQ(depth=1, port=5555)}))
         
         f.add_feed(Kraken(
             channels=[L2_BOOK, TRADES], 
             pairs=kraken_tickers, 
             callbacks={
                 TRADES: TradeZMQ(port=5555), 
-                L2_BOOK: BookZMQ(depth=1, port=5556)}))
+                L2_BOOK: BookZMQ(depth=1, port=5555)}))
         
         f.add_feed(Binance(
             channels=[L2_BOOK, TRADES], 
             pairs=binance_tickers, 
             callbacks={
                 TRADES: TradeZMQ(port=5555), 
-                L2_BOOK: BookZMQ(depth=1, port=5556)}))
+                L2_BOOK: BookZMQ(depth=1, port=5555)}))
 
         f.run()
 
     finally:
-        p1.terminate()
-        p2.terminate()
+        p.terminate()
         
         # save trades and quotes tables to disk
         data_path = "d:/repos/cryptoq/data"
