c68bc116c6f4012956fe3c493648fdf7010bace2
Adam <30326744+AlgoTrader5@users.noreply.github.com>
2019-08-10 16:02:22 -0500

    Update start_md.bat
    config file name typo

--- start_md.bat
+++ start_md.bat
@@ -1,3 +1,3 @@
 d:
 cd D:\repos\cryptoq
-python -m bin.cryptoq --port 5002 --depth 1 --config D:\repos\conf\subscriptions.yaml
+python -m bin.cryptoq --port 5002 --depth 1 --config D:\repos\cryptoq\conf\subscriptions.yaml
