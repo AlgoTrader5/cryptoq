30b61d7ee98c4a0a16b36b72d45a960344e700dd
Adam <30326744+AlgoTrader5@users.noreply.github.com>
2019-08-19 19:26:37 -0500

    Update cryptoq.py

--- bin/cryptoq.py
+++ bin/cryptoq.py
@@ -12,6 +12,7 @@ from cryptofeed import FeedHandler
 from cryptofeed.exchanges import Coinbase, Kraken, Binance, Poloniex
 from cryptofeed.defines import TRADES, L2_BOOK
 
+
 from utils import read_cfg, trade_convert, book_convert
 
 
@@ -20,16 +21,24 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-p", "--port", help='QConnection port')
 parser.add_argument("-d", "--depth", help='Order Book depth')
 parser.add_argument("-c", "--config", help='path to the config file')
+parser.add_argument("-k", "--kdbport", default=5555, help='ZMQ port for kdb+ capture')
+parser.add_argument("-g", "--guiport", default=5556, help='ZMQ port for gui')
 args = parser.parse_args()
 
 PORT = int(args.port)
 DEPTH = int(args.depth)
 CONFIG = args.config
+KDBPORT = int(args.kdbport)
+GUIPORT = int(args.guiport)
 
 # create connection object
 q = qconnection.QConnection(host='localhost', port=PORT, pandas=True)
 # initialize connection
 q.open()
+
+async def ohlcv(data=None):
+	print(data)
+
 
 def receiver(port):
 	ctx = zmq.Context.instance()
@@ -39,10 +48,13 @@ def receiver(port):
 
 	while True:
 		data = s.recv_string()
+		
 		if data[0] == "b":
 			qStr = book_convert(data, DEPTH)
-		else:
+		elif data[0] == "t":
 			qStr = trade_convert(data)
+		else:
+			print("Cannot recognize data message")
 		try:
 			q.sendSync(qStr, param=None)
 		except QException as e:
@@ -55,38 +67,39 @@ def main():
 	
 	subscriptions = read_cfg(CONFIG)
 	try:
-		p = Process(target=receiver, args=(5555,))
+		p = Process(target=receiver, args=(KDBPORT,))
 		p.start()
 
 		f = FeedHandler()
+
 		
 		f.add_feed(Coinbase(
 			channels=[L2_BOOK, TRADES], 
 			pairs=subscriptions['coinbase'], 
 			callbacks={
-				TRADES: [TradeZMQ(port=5555), TradeZMQ(port=5556)],
-				L2_BOOK: [BookZMQ(depth=DEPTH, port=5555), BookZMQ(depth=DEPTH, port=5556)]}))
+				TRADES: [TradeZMQ(port=KDBPORT), TradeZMQ(port=GUIPORT)],
+				L2_BOOK: [BookZMQ(depth=DEPTH, port=KDBPORT), BookZMQ(depth=DEPTH, port=GUIPORT)]}))
 		
 		f.add_feed(Kraken(
 			channels=[L2_BOOK, TRADES], 
 			pairs=subscriptions['kraken'], 
 			callbacks={
-				TRADES: [TradeZMQ(port=5555), TradeZMQ(port=5556)],
-				L2_BOOK: [BookZMQ(depth=DEPTH, port=5555), BookZMQ(depth=DEPTH, port=5556)]}))
+				TRADES: [TradeZMQ(port=KDBPORT), TradeZMQ(port=GUIPORT)],
+				L2_BOOK: [BookZMQ(depth=DEPTH, port=KDBPORT), BookZMQ(depth=DEPTH, port=GUIPORT)]}))
 		
 		f.add_feed(Binance(
 			channels=[L2_BOOK, TRADES], 
 			pairs=subscriptions['binance'], 
 			callbacks={
-				TRADES: [TradeZMQ(port=5555), TradeZMQ(port=5556)],
-				L2_BOOK: [BookZMQ(depth=DEPTH, port=5555), BookZMQ(depth=DEPTH, port=5556)]}))
+				TRADES: [TradeZMQ(port=KDBPORT), TradeZMQ(port=GUIPORT)],
+				L2_BOOK: [BookZMQ(depth=DEPTH, port=KDBPORT), BookZMQ(depth=DEPTH, port=GUIPORT)]}))
 
 		f.add_feed(Poloniex(
 			channels=[L2_BOOK, TRADES], 
 			pairs=subscriptions['poloniex'], 
 			callbacks={
-				TRADES: [TradeZMQ(port=5555), TradeZMQ(port=5556)],
-				L2_BOOK: [BookZMQ(depth=DEPTH, port=5555), BookZMQ(depth=DEPTH, port=5556)]}))
+				TRADES: [TradeZMQ(port=KDBPORT), TradeZMQ(port=GUIPORT)],
+				L2_BOOK: [BookZMQ(depth=DEPTH, port=KDBPORT), BookZMQ(depth=DEPTH, port=GUIPORT)]}))
 
 		f.run()
 
@@ -94,7 +107,7 @@ def main():
 		p.terminate()
 		
 		# save trades and quotes tables to disk
-		data_path = "c:/repos/cryptoq/data"
+		data_path = "d:/repos/cryptoq/data"
 		q.sendSync(f"`:{data_path}/trades set trades")
 		q.sendSync(f"`:{data_path}/quotes set quotes")
 		q.close()
