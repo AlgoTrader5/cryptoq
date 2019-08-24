468a4665822c071c2588d8d4062e3a5e9e533516
Adam <30326744+AlgoTrader5@users.noreply.github.com>
2019-08-24 18:17:04 -0500

    Update load_reference_data.py

--- load_reference_data.py
+++ load_reference_data.py
@@ -5,28 +5,27 @@ from qpython.qtype import QException
 
 # parse args from command line
 parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--port", help='QConnection port')
+parser.add_argument("-p", "--port", type=int, help='QConnection port')
 args = parser.parse_args()
 
 # create connection object
-q = qconnection.QConnection(host='localhost', port=int(args.port), pandas=True)
+q = qconnection.QConnection(host='localhost', port=args.port, pandas=True)
 # initialize connection
 q.open()
-print(q)
 print(f"IPC version: {q.protocol_version}. Is connected: {q.is_connected()}")
-
+from cryptofeed.pairs import gen_pair
 
 def main():
-	subscriptions = read_cfg("conf//subscriptions.yaml")
-    
-	r = Rest()
+    	pairs = gen_pair()
+	print("pairs:", pairs)
+# 	r = Rest()
 	
-	rest_clients = {
-		'coinbase': r.coinbase,
-		'binance': r.binance,
-		'kraken': r.kraken,
-		'poloniex': r.poloniex
-	}
+# 	rest_clients = {
+# 		'coinbase': r.coinbase,
+# 		'binance': r.binance,
+# 		'kraken': r.kraken,
+# 		'poloniex': r.poloniex
+# 	}
 
 if __name__ in "__main__":
 	main()
