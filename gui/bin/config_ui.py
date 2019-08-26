1ae765c80a445b0371ba19664a026b9a1b637970
Adam <30326744+AlgoTrader5@users.noreply.github.com>
2019-08-26 11:43:44 -0500

    Update config_ui.py

--- gui/bin/config_ui.py
+++ gui/bin/config_ui.py
@@ -1,6 +1,6 @@
- from gui.main_window import MainWindow
+from gui.main_window import MainWindow
  
- def main():
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", type=int, default=5002, help='QConnection port')
     args = parser.parse_args()
