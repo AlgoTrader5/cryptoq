b090402f0c3f79c9ec237a39f5b784cfea92bc35
Adam <30326744+AlgoTrader5@users.noreply.github.com>
2019-08-26 12:15:38 -0500

    Update config_ui.py

--- gui/bin/config_ui.py
+++ gui/bin/config_ui.py
@@ -3,11 +3,12 @@ from gui.main_window import MainWindow
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", type=int, default=5002, help='QConnection port')
+    parser.add_argument("-c", "--config", help='subscriptions config yaml file')
     args = parser.parse_args()
     
     app = QtWidgets.QApplication([])
 
-    win = MainWindow(port=args.port)
+    win = MainWindow(port=args.port, config=args.config)
     win.resize(1000, 500)
     win.show()
 
