39e02ca35a12297ec7c5d79ed23dfa2a01c37df7
Adam <30326744+AlgoTrader5@users.noreply.github.com>
2019-08-27 21:18:08 -0500

    Update load_refdata.py

--- utils/load_refdata.py
+++ utils/load_refdata.py
@@ -27,52 +27,61 @@ print(f"IPC version: {q.protocol_version}. Is connected: {q.is_connected()}")
 
 
 async def async_client(exchange):
-	client = getattr(ccxta, exchange)()
-	markets = await client.load_markets()
-	await client.close()
-	return {exchange: markets}
+    client = getattr(ccxta, exchange)()
+    markets = await client.load_markets()
+    await client.close()
+    return {exchange: markets}
 
 
 async def multi_tickers(exchanges):
-	input_coroutines = [async_client(exchange) for exchange in exchanges]
-	markets = await asyncio.gather(*input_coroutines, return_exceptions=True)
-	return markets
+    input_coroutines = [async_client(exchange) for exchange in exchanges]
+    markets = await asyncio.gather(*input_coroutines, return_exceptions=True)
+    return markets
 
 
 def insert_data(exch, sym, refdata):
-	makerFee = refdata['maker']
-	takerFee = refdata['taker']
-	minTick = refdata['limits']['price']['min']
-	minSize = refdata['limits']['amount']['min']
-	
-	qStr = f"`refdata insert (`$\"{sym}\";`$\"{exch}\";" \
-			f"`float${minTick};`float${minSize};" \
-			f"`float${makerFee};`float${takerFee})"
-	try:
-		q.sendSync(qStr, param=None)
-	except QException as e:
-		print(f"Error executing query {qStr} against server. {e}")
+    try:
+        makerFee = refdata['maker'] if refdata['maker'] else 0.0
+        takerFee = refdata['taker'] if refdata['taker'] else 0.0
+        minTick = refdata['limits']['price']['min'] \
+                if refdata['limits']['price']['min'] else 0.0
+        minSize = refdata['limits']['amount']['min'] \
+                if refdata['limits']['amount']['min'] else 0.0
+    except KeyError as e:
+        minTick = 0.0
+        minSize = 0.0
+        print(f"couln't find minTick/minSize params for {sym} {exch}")
+    
+    qStr = f"`refdata insert (`$\"{sym}\";`$\"{exch}\";" \
+            f"`float${minTick};`float${minSize};" \
+            f"`float${makerFee};`float${takerFee})"
+    try:
+        q.sendSync(qStr, param=None)
+    except QException as e:
+        print(f"Error executing query {qStr} against server. {e}")
 
-		
+        
 if __name__ == '__main__':
-	from pprint import pprint
+    from pprint import pprint
 
-	# Consider review request rate limit in the methods you call
-	exchanges = [
-		"coinex", "bitmex", "bittrex", "bitfinex", "poloniex", "hitbtc", 
-		"coinbasepro", "kraken", "binance", "bitstamp", "gemini",
-		"okex", "kucoin", "exx"
-	]
+    # Consider review request rate limit in the methods you call
+    exchanges = [
+        "coinex", "bitmex", "bittrex", "bitfinex", "poloniex", "hitbtc", 
+        "coinbasepro", "kraken", "binance", "bitstamp", "gemini",
+        "okex", "exx"
+    ]
 
-	tic = time.time()
-	markets = asyncio.get_event_loop().run_until_complete(multi_tickers(exchanges))
-	print("async call spend:", time.time() - tic)
-	
-	# write to kdb
-	for market in markets:
-		for exchange, refdata in market.items():
-			for sym, ref in refdata.items():
-				insert_data(exchange, sym, ref)
-	
-	
+    tic = time.time()
+    markets = asyncio.get_event_loop().run_until_complete(multi_tickers(exchanges))
+    print("async call spend:", time.time() - tic)
+
+    time.sleep(1)
+    
+    # write to kdb
+    for market in markets:
+        for exchange, refdata in market.items():
+            for sym, ref in refdata.items():
+                insert_data(exchange, sym, ref)
+    
+    
 
