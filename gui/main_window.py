ce969f988837c3e0253aad758ec49d60fd368c11
Adam <30326744+AlgoTrader5@users.noreply.github.com>
2019-09-22 17:00:51 -0500

    Update main_window.py

--- gui/main_window.py
+++ gui/main_window.py
@@ -16,16 +16,14 @@ from contracts_tab import ContractsTab
 from cryptofeed.defines import BITSTAMP, BITFINEX, COINBASE, FTX, GEMINI, HITBTC, POLONIEX, KRAKEN, BINANCE, EXX, HUOBI, HUOBI_US, OKCOIN, OKEX, COINBENE, BYBIT
 from cryptofeed.pairs import gen_pairs
 
+
+root = os.path.dirname(os.getcwd())
+
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self, port, config):
+    def __init__(self, config):
         super(MainWindow, self).__init__()
-
-        # windows
-        self.central_widget = None
-        self.exchange_window = None
-        self.contracts_window = None
-
-        self._client_dict = {
+        self.config = self._read_config(f'{root}\\conf\\{config}')
+        self.markets = {
             'binance': gen_pairs(BINANCE),
             'bitfinex': gen_pairs(BITFINEX),
             'bitstamp': gen_pairs(BITSTAMP),
@@ -42,8 +40,10 @@ class MainWindow(QtWidgets.QMainWindow):
             'okex': gen_pairs(OKEX),
             'poloniex': gen_pairs(POLONIEX)
         }
-
-        self._subscription_dict = self._read_config(config)
+        # windows
+        self.central_widget = None
+        self.exchange_window = None
+        self.contracts_window = None
         
         # 1. set up gui windows
         self.setGeometry(50, 50, 600, 400)
@@ -81,7 +81,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.statusBar().showMessage(message)
 
     def open_proj_folder(self):
-        webbrowser.open('C:/repos/')
+        webbrowser.open(root)
 
     def closeEvent(self, a0: QtGui.QCloseEvent):
         print('close main window')
@@ -100,12 +100,14 @@ class MainWindow(QtWidgets.QMainWindow):
     def init_menu(self):
         menubar = self.menuBar()
         sysMenu = menubar.addMenu('File')
+        
         # open folder
         sys_folderAction = QtWidgets.QAction('Folder', self)
         sys_folderAction.setStatusTip('Open_Folder')
         sys_folderAction.triggered.connect(self.open_proj_folder)
         sysMenu.addAction(sys_folderAction)
         sysMenu.addSeparator()
+        
         # sys|exit
         sys_exitAction = QtWidgets.QAction('Exit', self)
         sys_exitAction.setShortcut('Ctrl+Q')
@@ -117,18 +119,18 @@ class MainWindow(QtWidgets.QMainWindow):
     def init_central_area(self):
         self.central_widget = QtWidgets.QWidget()
         hbox = QtWidgets.QHBoxLayout()
+        
         # -------------------------------- bottom Left ------------------------------------------#
         tab_widget = QtWidgets.QTabWidget()
         tab1 = QtWidgets.QWidget()          # contract
         tab_widget.addTab(tab1, 'Contract')
 
         # --------------------------------  CONTRACT TAB ------------------------------------------#
-        self.contract_tab = ContractsTab(self._client_dict, self._subscription_dict)
+        self.contract_tab = ContractsTab(self.markets, self.config)
         contract_tab_layout = QtWidgets.QHBoxLayout()
         contract_tab_layout.addWidget(self.contract_tab)
         tab1.setLayout(contract_tab_layout)
-
-
+        
         hbox.addWidget(tab_widget)
         self.central_widget.setLayout(hbox)
         self.setCentralWidget(self.central_widget)
