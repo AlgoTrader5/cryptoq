f4159f29a24db5cf3c33daa38fea7dcffa57f77e
Adam <30326744+AlgoTrader5@users.noreply.github.com>
2019-09-28 19:31:38 -0500

    Update start_md.bat

--- start_md.bat
+++ start_md.bat
@@ -1,3 +1,3 @@
 d:
 cd D:\repos\cryptoq
-python -m bin.cryptoq --q-port 5002 --kdb-port 5555 --gui-port 5556 --depth 1 --config conf\subscriptions.yaml
+python -m bin.cryptoq --q-port 5002 --kdb-port 5555 --kdb-depth 5 --gui-port 5556 --gui-depth 1 --config conf\subscriptions.yaml
