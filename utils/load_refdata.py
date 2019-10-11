32a1dcc389a3c3643fd9e5a089743d846c522b38
AlgoTrader5 <AlgoTrader5@users.noreply.github.com>
2019-10-10 21:19:37 -0500

    added kdb client class

--- utils/load_refdata.py
+++ utils/load_refdata.py
@@ -4,6 +4,7 @@ import sys
 import time
 import argparse
 from pprint import pprint
+
 import asyncio
 import ccxt
 import ccxt.async_support as ccxta  # noqa: E402
@@ -17,14 +18,38 @@ sys.path.append(root + '/python')
 
 # parse args from command line
 parser = argparse.ArgumentParser()
-parser.add_argument("--q-port", dest="q_port", type=int, default=5002, help='QConnection port')
+parser.add_argument("--q-host", dest="q_host", type=str, default='localhost', help='QConnection host')
+parser.add_argument("--q-port", dest="q_port", type=int, default=5002,        help='QConnection port')
 args = parser.parse_args()
 
-# create connection object
-q = qconnection.QConnection(host='localhost', port=args.q_port, pandas=True)
-# initialize connection
-q.open()
-print(f"IPC version: {q.protocol_version}. Is connected: {q.is_connected()}")
+
+
+class KdbConfigDb:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+        self.q = qconnection.QConnection(self.host, self.port, pandas=True)
+        self.q.open()
+        print(f"IPC version: {self.q.protocol_version}. Is connected: {self.q.is_connected()}")
+
+    def insert_refdata(self, data):
+        try:
+            qStr = f"`refdata insert (`$\"{data['instrument']}\";`$\"{data['exchange']}\";" \
+                    f"`float${data['minTick']};`float${data['minSize']};" \
+                    f"`float${data['makerFee']};`float${data['takerFee']})"
+            self.q.sendSync(qStr, param=None)
+        except QException as e:
+            print(f"Error executing query {qStr} against server. {e}")
+
+    def send_sync(self, data):
+        try:
+            self.q.sendSync(data, param=None)
+        except QException as e:
+            print(f"Error executing query {data} against server. {e}")
+
+    def close(self):
+        self.q.close()
+
 
 
 async def async_client(exchange):
@@ -40,8 +65,8 @@ async def multi_tickers(exchanges):
     return markets
 
 
-def insert_data(exch, sym, refdata):
-    if exch=='kraken' and '.d' in sym:
+def normalize_data(exchange, instrument, refdata):
+    if exchange=='kraken' and '.d' in instrument:
         return
     try:
         makerFee = refdata['maker'] if not isinstance(refdata['maker'], type(None)) else 0.0
@@ -59,21 +84,20 @@ def insert_data(exch, sym, refdata):
         minTick = 0.0
         minSize = 0.0
 
-
-    qStr = f"`refdata insert (`$\"{sym}\";`$\"{exch}\";" \
-            f"`float${minTick};`float${minSize};" \
-            f"`float${makerFee};`float${takerFee})"
+    return {
+        'exchange': exchange,
+        'instrument': instrument, 
+        'minTick': minTick,
+        'minSize': minSize,
+        'makerFee': makerFee,
+        'takerFee': takerFee
+    }
     
-    try:
-        q.sendSync(qStr, param=None)
-    except QException as e:
-        print(f"Error executing query {qStr} against server. {e}")
-
-        
-
 
 def main():
-    from pprint import pprint
+    
+
+    config_db = KdbConfigDb(args.q_host, args.q_port)
 
     # Consider review request rate limit in the methods you call
     exchanges = [
@@ -91,10 +115,12 @@ def main():
     for market in markets:
         for exchange, refdata in market.items():
             for sym, ref in refdata.items():
-                insert_data(exchange, sym, ref)
+                data = normalize_data(exchange, sym, ref)
+                if data:
+                    config_db.insert_refdata(data)
     
-    q.sendSync("show `sym xdesc select count sym by exch from refdata")
-    q.close()
+    config_db.send_sync("show `sym xdesc select count sym by exch from refdata")
+    config_db.close()
     time.sleep(3)
     
 if __name__ == '__main__':
