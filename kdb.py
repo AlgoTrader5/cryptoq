51f2cf0a3b8a5f57e7548bcd3e8d75f84e228718
Adam <30326744+AlgoTrader5@users.noreply.github.com>
2020-11-27 19:58:35 -0600

    Update kdb.py
    fix whitespaces

--- kdb.py
+++ kdb.py
@@ -4,8 +4,10 @@ from qpython import qconnection
 from qpython.qtype import QException
 
 from cryptofeed.defines import TRADES, FUNDING, TICKER, OPEN_INTEREST, L2_BOOK
-from cryptofeed.backends.backend import BackendBookCallback, BackendBookDeltaCallback, BackendTradeCallback, BackendTickerCallback, BackendFundingCallback, BackendOpenInterestCallback
-
+from cryptofeed.backends.backend import (BackendBookCallback, BackendBookDeltaCallback, 
+                                         BackendTradeCallback, BackendTickerCallback, 
+                                         BackendFundingCallback, BackendOpenInterestCallback)
+    
 
 class KdbCallback:
     def __init__(self, host='127.0.0.1', key=None, numeric_type=float, **kwargs):
@@ -17,26 +19,30 @@ class KdbCallback:
     async def write(self, feed, pair, delta, timestamp):
         print(feed, pair, delta, timestamp)
 
-
-
+        
 class BookDeltaKdb(KdbCallback, BackendBookDeltaCallback):
     default_key = L2_BOOK
 
+    
 class BookKdb(KdbCallback, BackendBookCallback):
     default_key = L2_BOOK
 
+    
 class TradeKdb(KdbCallback, BackendTradeCallback):
     default_key = TRADES
 
+    
 class TradeKdb(KdbCallback, BackendTradeCallback):
     default_key = TRADES
 
-
+    
 class FundingKdb(KdbCallback, BackendFundingCallback):
     default_key = FUNDING
 
+    
 class TickerKdb(KdbCallback, BackendTickerCallback):
     default_key = TICKER
 
+    
 class OpenInterestKdb(KdbCallback, BackendOpenInterestCallback):
     default_key = OPEN_INTEREST
